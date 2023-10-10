588d2829fbfbc8ed50a9dc3a815c88bd0c7dbf0f
divineamunega <114052520+divineamunega@users.noreply.github.com>
2023-10-10 02:02:40 +0100

    Feat: Made Nav responsive

--- quiz/src/components/NavLinks.jsx
+++ quiz/src/components/NavLinks.jsx
@@ -1,11 +1,9 @@
 import { Link } from "react-router-dom";
 
 export default function NavLinks({ children }) {
-  return (
-    <li>
-      <Link className="text-[1.6rem] py-4 px-8" to="/">
-        {children}
-      </Link>
-    </li>
-  );
+	return (
+		<li className="text-[1.8rem] py-4 px-8 mx-2 hover:bg-cta-bg-color hover:text-white transition-all ease-in-out  delay-10 duration-500 hover:opacity-90 rounded my-5 cursor-pointer">
+			<Link to="/">{children}</Link>
+		</li>
+	);
 }
